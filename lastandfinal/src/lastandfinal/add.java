1476bd785f3ee7dec0eafdb6c3e01ad29f824bd1
shruthiyasam <shruthiyasam83@gmail.com>
2019-05-18 13:11:57 +0530

    added

---
+++ lastandfinal/src/lastandfinal/add.java
@@ -0,0 +1,14 @@
+package lastandfinal;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=90;
+		int b=46;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
